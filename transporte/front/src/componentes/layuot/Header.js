1d92d21387935b197498cf3ce7492aee46d298a1
Carlos Ramirez <carlitoxramirex@hotmail.com>
2023-07-24 15:21:30 -0300

    tarea react m4u1

---
+++ transporte/front/src/componentes/layuot/Header.js
@@ -0,0 +1,15 @@
+import Nav from "./Nav";
+
+const Header = (props) => {
+    return (
+
+    <>
+        <Nav/>
+        <h1>hola soy la cabezera</h1>
+        <p>lorem</p>
+
+    </>
+    )
+}
+
+export default Header;
---
+++ transporte/front/src/componentes/layuot/Nav.js
@@ -0,0 +1,13 @@
+import Header from "./Header"
+
+const Nav = (props) => {
+    return (
+        <nav>
+            <h1>
+                hola soy la navegacion
+            </h1>
+        </nav>
+    )
+}
+
+export default Nav;
